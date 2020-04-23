7a214f721fbd332adcd0ac41a438f7eb98e45e71
Stephanie smith <smithse4@gmail.com>
2020-04-22 20:56:03 -0400

    added html routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,27 @@
 const db = require("../models");
 
 module.exports = function (app) {
+  app.get("/api/workouts", function (req, res) {
+    db.find()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.post("/api/workouts", function (req, res) {
+    db.create({})
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
 
-    
-}
+
+  
+};
